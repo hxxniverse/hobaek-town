6068d94fec3bbd51fd7e47f1ca88005fac7e1ea7
donghune <reona7140@naver.com>
2024-05-24 15:02:43 +0900

    유저 주식 보유 테이블 키 타입 변경 주식 거래 히스토리 테이블 추가

--- hobeak-town/src/main/kotlin/io/github/hxxniverse/hobeaktown/feature/stock/StockConfig.kt
+++ hobeak-town/src/main/kotlin/io/github/hxxniverse/hobeaktown/feature/stock/StockConfig.kt
@@ -1,10 +1,5 @@
 package io.github.hxxniverse.hobeaktown.feature.stock
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class StockConfig(
     val fluctuationTime: Int = 1,
-    val maxFluctuation: Int = 30,
-    val minFluctuation: Int = 30,
 )
